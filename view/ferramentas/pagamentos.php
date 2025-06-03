400fbbe25ac6f92033d882e5c69e60b6fa343b3e
LuizHBF <145623728+LuizHBF@users.noreply.github.com>
2025-06-02 21:17:37 -0300

    Update favicon pagamentos.php

--- view/ferramentas/pagamentos.php
+++ view/ferramentas/pagamentos.php
@@ -11,6 +11,7 @@ include ('../../controller/controlar_pagamentos.php');
 	<meta name="author" content="Rodrigo Jordão, Luiz Henrique, Moysés Souza, Guilherme Oliveira">
 	<meta name="generator" content="Hugo 0.145.0">
 	<title>Pagamentos</title>
+	<link rel="shortcut icon" href="./img/favicon.svg" type="image/x-icon">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="../../view/styles/pagamentos.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
